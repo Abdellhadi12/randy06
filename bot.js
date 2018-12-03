562e441b7cca5046e0f863514852002d268daeee
rand1y <45459672+rand1y@users.noreply.github.com>
2018-12-03 13:28:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ client.on("message", message => {
    
    }
    });  
-   const welcome = JSON.parse(fs.readFileSync('./welcomer.json' , 'utf8'));
+   let welcome = JSON.parse(fs.readFileSync('./welcomer.json' , 'utf8'));
  
 client.on('message', message => {
            if (!message.channel.guild) return;
